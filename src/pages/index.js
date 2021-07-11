a5215367474765b94d12557a684dbc3ca2c25771
kai weiss <kaiweiss@Mac-mini-von-kai.local>
2021-07-11 15:04:55 +0200

    next step react plugin image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-    <p>I'm making this ....</p>
+    <p>I'm making this ..hot..</p>
+    <StaticImage
+        alt="Terminator coming back to buy Bitcoin"
+        src="../images/arnold-buying-bitcoin-cheap.jpeg"
+      />
   </Layout>
   )
 }
