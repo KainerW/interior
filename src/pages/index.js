8976ad8b213c97cc7ede7f8b41adec573a3d064e
kai weiss <kaiweiss@Mac-mini-von-kai.local>
2021-07-11 15:40:44 +0200

    changed image and text again and again

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Interior Design München/Köln">
-    <p>Wohnen und wohlfühlen - mit <a href="https-//pbshomes.de">pbshomes.de</a></p>
+    <p>Wohnen und wohlfühlen mit <a href="https://pbshomes.de">pbshomes.de</a></p>
     <StaticImage
         alt="Wohnzimmer Interior Design aus München"
         src="../images/pbshomes-interior-design-feng-shui.jpg"
